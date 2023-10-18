2c3639674b25fe2ab057e096158b6f38ec696453
Noida-Nataly <116755055+Noida-Nataly@users.noreply.github.com>
2023-10-18 23:34:44 +0300

    Update AboutMe.js

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,10 +8,10 @@ export default function AboutMe ({}) {
                 <div className="about-me__infoblok">
                     <h3 className="about-me__name heading-text">Наталия</h3>
                     <p className="about-me__info">Фронтенд-разработчик</p>
-                    <p className="about-me__text">Ты скажешь эта жизнь — одно мгновенье.<br>
-                                                    Ее цени, в ней черпай вдохновенье.<br>
-                                                    Как проведешь ее, так и пройдет,<br>
-                                                    Не забывай: она — твое творенье.<br>
+                    <p className="about-me__text">Ты скажешь эта жизнь — одно мгновенье.<br/>
+                                                    Ее цени, в ней черпай вдохновенье.<br/>
+                                                    Как проведешь ее, так и пройдет,<br/>
+                                                    Не забывай: она — твое творенье.<br/>
                                                                         Омар Хайам</p>
                     <Link className="about-me__link link"  target = "_blank" to="https://github.com/Noida-Nataly" >GitHub</Link>
                 </div>

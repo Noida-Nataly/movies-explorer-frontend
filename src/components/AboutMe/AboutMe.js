a81c9379b2dfbffd33b7402481a59ffadbf4108a
Noida-Nataly <116755055+Noida-Nataly@users.noreply.github.com>
2023-10-18 23:28:14 +0300

    Update AboutMe.js

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -7,13 +7,12 @@ export default function AboutMe ({}) {
             <div className="about-me__container">
                 <div className="about-me__infoblok">
                     <h3 className="about-me__name heading-text">Наталия</h3>
-                    <p className="about-me__info">Фронтенд-разработчик, 30 лет</p>
-                    <p className="about-me__text">Я родился и живу в Саратове,
-                        закончил факультет экономики СГУ. У меня есть жена
-                        и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом.
-                        Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».
-                        После того, как прошёл курс по веб-разработке,
-                        начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                    <p className="about-me__info">Фронтенд-разработчик</p>
+                    <p className="about-me__text">Ты скажешь эта жизнь — одно мгновенье.<br>
+                                                    Ее цени, в ней черпай вдохновенье.<br>
+                                                    Как проведешь ее, так и пройдет,<br>
+                                                    Не забывай: она — твое творенье.<br>
+                                                                        Омар Хайам</p>
                     <Link className="about-me__link link"  target = "_blank" to="https://github.com/Noida-Nataly" >GitHub</Link>
                 </div>
                 <img className="about-me__avatar"
